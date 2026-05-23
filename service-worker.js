c18c8fd523d3b19a3386c2cfe61783a40ae5f016
Ramateur <rasmus2578@outlook.dk>
2026-05-23 22:24:28 +0200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,15 +1,33 @@
+const CACHE_NAME = "v2";
+
+const FILES_TO_CACHE = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./icon-192.png",
+  "./icon-512.png"
+];
+
 self.addEventListener("install", event => {
   event.waitUntil(
-    caches.open("v1").then(cache => {
-      return cache.addAll([
-        "./",
-        "./index.html",
-        "./manifest.json",
-        "./icon-192.png",
-        "./icon-512.png"
-      ]);
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(FILES_TO_CACHE);
     })
   );
+  self.skipWaiting(); // Activate new SW immediately without waiting
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys
+          .filter(key => key !== CACHE_NAME) // Delete all old caches
+          .map(key => caches.delete(key))
+      );
+    })
+  );
+  self.clients.claim(); // Take control of all open tabs immediately
 });
 
 self.addEventListener("fetch", event => {
